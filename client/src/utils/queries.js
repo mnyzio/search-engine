2fcd6caff95d2c9a57dbe5c63fa0a7b58623af64
mnyzio <nyziu84@gmail.com>
2023-05-21 18:52:04 -0400

    client side queries and mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,15 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query me {
+    me {
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        description
+      }
+    }
+  }
+`;
